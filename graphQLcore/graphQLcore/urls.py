6a1cf40e3b0ef804834db2fb5f17a6d078851b1c
Unknown <bryan.robitaille@tbs-sct.gc.ca>
2018-02-28 16:18:18 -0500

    changed url endpoint for graphiQL

--- graphQLcore/graphQLcore/urls.py
+++ graphQLcore/graphQLcore/urls.py
@@ -20,5 +20,5 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphiql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
