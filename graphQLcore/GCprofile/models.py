3f23add9b324a111108826c4270e64c220b80ad2
Unknown <bryan.robitaille@tbs-sct.gc.ca>
2018-03-05 16:57:32 -0500

    Added mutations for Department, OrtTier, and Address to grpahQL API

--- graphQLcore/GCprofile/models.py
+++ graphQLcore/GCprofile/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     mobile_phone = models.CharField(null=True, blank=True, max_length=15)
     office_phone = models.CharField(null=True, blank=True, max_length=15)
     address = models.ForeignKey('Address', null=True, on_delete=models.CASCADE)
-    title_en = models.CharField(null=False, blank=False, max_length=150)
-    title_fr = models.CharField(null=False, blank=False, max_length=150)
+    title_en = models.CharField(null=True, blank=True, max_length=150)
+    title_fr = models.CharField(null=True, blank=True, max_length=150)
     supervisor = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
     org = models.ForeignKey('OrgTier', null=True, blank=True, on_delete=models.SET_NULL)
 
@@ -33,7 +33,7 @@ class Address(models.Model):
 class OrgTier(models.Model):
     name_en = models.CharField(null=False, blank=False, max_length=150)
     name_fr = models.CharField(null=False, blank=False, max_length=150)
-    department = models.ForeignKey('Department', null=True, blank=False, on_delete=models.CASCADE)
+    department = models.ForeignKey('Department', null=False, blank=False, on_delete=models.CASCADE)
     ownerID = models.ForeignKey('Profile', null=True, blank=False, on_delete=models.ProtectedError)
 
     def __str__(self):
--- graphQLcore/GCprofile/schema.py
+++ graphQLcore/GCprofile/schema.py
@@ -25,6 +25,8 @@ class DepartmentType(DjangoObjectType):
 
 
 class ProfileOptionalInput(graphene.InputObjectType):
+    title_en = graphene.String(required=False, default_value=None)
+    title_fr = graphene.String(required=False, default_value=None)
     avatar = graphene.String(required=False, default_value=None)
     mobile_phone = graphene.String(required=False, default_value=None)
     office_phone = graphene.String(required=False, default_value=None)
@@ -50,18 +52,15 @@ class CreateProfile(graphene.Mutation):
         gc_id = graphene.String()
         name = graphene.String()
         email = graphene.String()
-        title_en = graphene.String()
-        title_fr = graphene.String()
         optional_data = ProfileOptionalInput(required=False)
 
+    @staticmethod
     def mutate(self, info, gc_id, name, email, title_en, title_fr, optional_data=None):
 
         profile = Profile(
             gcID=gc_id,
             name=name,
             email=email,
-            title_en=title_en,
-            title_fr=title_fr,
         )
 
         if optional_data is None:
@@ -72,6 +71,8 @@ class CreateProfile(graphene.Mutation):
             profile.avatar = optional_data.avatar
             profile.mobile_phone = optional_data.mobile_phone
             profile.office_phone = optional_data.office_phone
+            profile.title_fr = optional_data.title_fr
+            profile.title_en = optional_data.title_en
 
             if optional_data.address_id is None:
                 profile.address = None
@@ -106,12 +107,118 @@ class CreateProfile(graphene.Mutation):
             org=profile.org,)
 
 
+class CreateDepartment(graphene.Mutation):
+    name_en = graphene.String()
+    name_fr = graphene.String()
+    acronym_fr = graphene.String()
+    acronym_en = graphene.String()
+
+    class Arguments:
+        name_en = graphene.String()
+        name_fr = graphene.String()
+        acronym_fr = graphene.String()
+        acronym_en = graphene.String()
+
+    @staticmethod
+    def mutate(self, info, name_en, name_fr, acronym_en, acronym_fr):
+        department = Department(
+            name_en=name_en,
+            name_fr=name_fr,
+            acronym_en=acronym_en,
+            acronym_fr=acronym_fr,
+        )
+        department.save()
+        return CreateDepartment(
+            name_en=department.name_en,
+            name_fr=department.name_fr,
+            acronym_fr=department.acronym_fr,
+            acronym_en=department.acronym_en,
+        )
+
+
+class CreateOrtTier(graphene.Mutation):
+    name_en = graphene.String()
+    name_fr = graphene.String()
+    department = graphene.Field(DepartmentType)
+    ownerID = graphene.Field(ProfileType)
+
+    class Arguments:
+        name_en = graphene.String()
+        name_fr = graphene.String()
+        department_id = graphene.Int()
+        owner_gc_id = graphene.String(required=False, default_value=None)
+
+    @staticmethod
+    def mutate(self, info, name_en, name_fr, department_id, owner_gc_id=None):
+
+        orgtier = OrgTier(
+            name_en=name_en,
+            name_fr=name_fr,
+        )
+
+        if owner_gc_id is None:
+            orgtier.ownerID = None
+        else:
+            orgtier.ownerID = Profile.objects.filter(gcID=owner_gc_id).first()
+            if not orgtier.ownerID:
+                raise Exception('Could not find Owner ID')
+        if department_id is None:
+            orgtier.department = None
+        else:
+            orgtier.department = Department.objects.filter(id=department_id).first()
+            if not orgtier.department:
+                raise Exception('Could not find Department ID')
+
+        orgtier.save()
+        return CreateOrtTier(
+            name_en=orgtier.name_en,
+            name_fr=orgtier.name_fr,
+            department=orgtier.department,
+            ownerID=orgtier.department
+        )
+
+
+class CreateAddress(graphene.Mutation):
+    street_address = graphene.String()
+    city = graphene.String()
+    province = graphene.String()
+    postal_code = graphene.String()
+    country = graphene.String(default_value="Canada")
+
+    class Arguments:
+        street_address = graphene.String()
+        city = graphene.String()
+        province = graphene.String()
+        postal_code = graphene.String()
+        country = graphene.String()
+
+    @staticmethod
+    def mutate(self, info, street_address, city, province, postal_code, country):
+        address = Address(
+            street_address=street_address,
+            city=city,
+            province=province,
+            postal_code=postal_code,
+            country=country,
+        )
+        address.save()
+
+        return CreateAddress(
+            street_address=address.street_address,
+            city=address.city,
+            province=address.province,
+            postal_code=address.postal_code,
+            country=address.country,
+        )
+
+
 class Query(graphene.ObjectType):
     profiles = graphene.List(ProfileType, search=graphene.String())
     addresses = graphene.List(AddressType)
     orgtiers = graphene.List(OrgTierType)
     departments = graphene.List(DepartmentType)
 
+    @staticmethod
     def resolve_profiles(self, info, search=None, **kwargs):
         if search is not None:
             filter = (
@@ -120,15 +227,21 @@ class Query(graphene.ObjectType):
             return Profile.objects.filter(filter)
         return Profile.objects.all()
 
+    @staticmethod
     def resolve_addresses(self, info, **kwargs):
         return Address.objects.all()
 
+    @staticmethod
     def resolve_orgtiers(self, info, **kwargs):
         return OrgTier.objects.all()
 
+    @staticmethod
     def resolve_departments(self, info, **kwargs):
         return Department.objects.all()
 
 
 class Mutation(graphene.ObjectType):
     create_profile = CreateProfile.Field()
+    create_department = CreateDepartment.Field()
+    create_org_tier = CreateOrtTier.Field()
+    create_address =CreateAddress.Field()
